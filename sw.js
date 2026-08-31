2f289687c40119af4fa8c5f8d301d1d9d2cb5185
Lali398 <korsoslala@gmail.com>
2026-08-31 18:19:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME        = 'beer-app-v32';
+const CACHE_NAME        = 'beer-app-v33';
 const API_CACHE_NAME    = 'beer-api-cache-v3';
 const SYNC_QUEUE_DB     = 'beer-sync-queue';
 const SYNC_TAG          = 'beer-sync-offline';
